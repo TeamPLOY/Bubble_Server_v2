77945656d5fc386fec14db1765122d3c25c94b88
한태영 <s46422574@gmail.com>
2024-10-04 19:59:44 +0900

    FEAT :: 이슈 템플릿 추가

---
+++ src/main/kotlin/com/ploy/bubble_server_v2/BubbleServerV2Application.kt
@@ -0,0 +1,11 @@
+package com.ploy.bubble_server_v2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BubbleServerV2Application
+
+fun main(args: Array<String>) {
+	runApplication<BubbleServerV2Application>(*args)
+}
---
+++ src/test/kotlin/com/ploy/bubble_server_v2/BubbleServerV2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ploy.bubble_server_v2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BubbleServerV2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
