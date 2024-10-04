da73bfcaf213451ab50ee2ad1c3eecda90a5bb15
한태영 <s46422574@gmail.com>
2024-10-04 09:16:28 +0900

    feat(#6) :: JwtProperties
    JwtProperties를 구현합니다.

--- src/main/kotlin/com/ploy/bubble_server_v2/common/jwt/JwtProperties.kt
+++ src/main/kotlin/com/ploy/bubble_server_v2/common/jwt/JwtProperties.kt
@@ -1,4 +1,13 @@
 package com.ploy.bubble_server_v2.common.jwt
 
-class JwtProperties {
-}
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+data class JwtProperties(
+    var issuer: String? = null,
+    var clientSecret: String? = null,
+    var tokenExpire: Int = 0,
+    var refreshTokenExpire: Int = 0
+)
