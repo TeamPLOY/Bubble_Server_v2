1440c0041111ec04f94783829de9ebf4273ed2a4
한태영 <s46422574@gmail.com>
2024-10-08 22:47:41 +0900

    test(#9) :: SwaggerConfigTest
    SwaggerConfigTest를 구현합니다.

--- src/test/kotlin/com/ploy/bubble_server_v2/common/config/SwaggerConfigTest.kt
+++ src/test/kotlin/com/ploy/bubble_server_v2/common/config/SwaggerConfigTest.kt
@@ -1,4 +1,20 @@
 package com.ploy.bubble_server_v2.common.config
 
+import io.swagger.v3.oas.models.OpenAPI
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
 class SwaggerConfigTest {
+
+    @Autowired
+    lateinit var swaggerConfig: SwaggerConfig
+
+    @Test
+    fun `customOpenAPI should be created`() {
+        val openAPI: OpenAPI = swaggerConfig.customOpenAPI()
+        assertNotNull(openAPI) // OpenAPI 객체가 null이 아님을 확인
+    }
 }
