c2f6caad8d770f63c18f7c9732962bea78c97457
한태영 <s46422574@gmail.com>
2024-10-08 22:13:54 +0900

    feat(#9) :: RestTemplateClient
    RestTemplateClient를 구현합니다.

--- src/main/kotlin/com/ploy/bubble_server_v2/common/config/RestTemplateClient.kt
+++ src/main/kotlin/com/ploy/bubble_server_v2/common/config/RestTemplateClient.kt
@@ -1,4 +1,14 @@
 package com.ploy.bubble_server_v2.common.config
 
-class RestTemplateClient {
+import org.springframework.boot.web.client.RestTemplateBuilder // RestTemplate을 빌드하는 데 사용되는 빌더 클래스
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+open class RestTemplateClient {
+    @Bean
+    open fun restTemplate(builder: RestTemplateBuilder): RestTemplate { // 메서드를 open으로 선언
+        return builder.build()
+    }
 }
