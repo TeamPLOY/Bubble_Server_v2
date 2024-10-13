d5838c5ea99ed5ca351ea9b7fce7d5b0eb5415c2
noahmik <s46422574@gmail.com>
2024-10-13 23:44:00 +0900

    feat(#1) :: application-db.yml
    application-db.yml 파일을 구현하고 redis, postgresql 관련된 의존성을 추가했습니다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.3.4"
-	id("io.spring.dependency-management") version "1.1.6"
+	kotlin("jvm") version "1.9.10"  // 최신 버전으로 교체하세요.
+	id("org.springframework.boot") version "3.2.0"  // Spring Boot 버전도 최신으로 교체하세요.
+	id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "com.ploy"
@@ -24,6 +23,14 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.postgresql:postgresql:42.6.0")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 kotlin {
