0f8ba5e94be623d8144384ca4a78c7fd76e16984
noahmik <s46422574@gmail.com>
2024-10-14 01:55:25 +0900

    feat(#4) :: BusinessException
    BusinessException를 구현합니다.

--- src/main/kotlin/com/ploy/bubble_server_v2/common/exception/BusinessException.kt
+++ src/main/kotlin/com/ploy/bubble_server_v2/common/exception/BusinessException.kt
@@ -1,4 +1,8 @@
 package com.ploy.bubble_server_v2.common.exception
 
-class BusinessException {
+class BusinessException(val errorCode: ErrorCode) : RuntimeException(errorCode.message) {
+    // 비즈니스 로직에서 발생한 에러를 식별하기 위한 에러 코드
+
+    // 생성자: 에러 코드를 인자로 받아 RuntimeException의 생성자에 메시지를 전달합니다.
 }
+
