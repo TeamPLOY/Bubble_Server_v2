5f53a9fa2a890cbaf78ec3f6bafb731f3e32f41a
YunChan-Oh <oyunchan9@gmail.com>
2025-01-16 23:03:03 +0900

    init

---
+++ src/main/java/com/ploy/bubble_server_v3/BubbleServerV3Application.java
@@ -0,0 +1,13 @@
+package com.ploy.bubble_server_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleServerV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BubbleServerV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ploy/bubble_server_v3/BubbleServerV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ploy.bubble_server_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubbleServerV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
