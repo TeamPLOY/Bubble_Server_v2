de942cb96eb38416d19b6c8057fc48360b874e31
noahmik <s46422574@gmail.com>
2025-01-23 17:03:01 +0900

    feat(#9) :: TokenResponse
    TokenResponse를 구현합니다

--- src/main/java/com/ploy/bubble_server_v3/common/jwt/dto/TokenResponse.java
+++ src/main/java/com/ploy/bubble_server_v3/common/jwt/dto/TokenResponse.java
@@ -1,2 +1,7 @@
-package com.ploy.bubble_server_v3.common.jwt.dto;public record TokenResponse() {
+package com.ploy.bubble_server_v3.common.jwt.dto;
+
+public record TokenResponse(
+        String accessToken,
+        String refreshToken
+) {
 }
