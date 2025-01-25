d3c64dff5a67f149dc94c3e72336d3ab197e106b
noahmik <s46422574@gmail.com>
2025-01-25 15:57:43 +0900

    feat(#11) :: BaseException
    커스텀 예외 처리용 BaseException 클래스 구현

--- src/main/java/com/ploy/bubble_server_v3/common/exception/BaseException.java
+++ src/main/java/com/ploy/bubble_server_v3/common/exception/BaseException.java
@@ -1,2 +1,17 @@
-package com.ploy.bubble_server_v3.common.exception;public class BaseException {
+package com.ploy.bubble_server_v3.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BaseException extends RuntimeException {
+    private final HttpStatus status;
+    private final String errorCode;
+
+    public BaseException(HttpStatus status, String errorCode, String message) {
+        super(message);
+        this.status = status;
+        this.errorCode = errorCode;
+    }
 }
+
