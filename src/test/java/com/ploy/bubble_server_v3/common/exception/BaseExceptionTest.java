79a5b5dba30a8ae9c9b22d6fe424cd6c24cf5e91
noahmik <s46422574@gmail.com>
2025-01-27 17:21:17 +0900

    test(#11) :: BaseExceptionTest
    BaseException 초기화 테스트 코드를 작성했습니다

--- src/test/java/com/ploy/bubble_server_v3/common/exception/BaseExceptionTest.java
+++ src/test/java/com/ploy/bubble_server_v3/common/exception/BaseExceptionTest.java
@@ -1,4 +1,24 @@
 package com.ploy.bubble_server_v3.common.exception;
 
-public class BaseException {
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseExceptionTest {
+
+    @Test
+    void BaseException_초기화_테스트() {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        String errorCode = "USER_NOT_FOUND";
+        String message = "유저를 찾을 수 없습니다.";
+
+        BaseException exception = new BaseException(status, errorCode, message);
+
+        assertEquals(status, exception.getStatus(), "HttpStatus가 올바르게 설정되어야 합니다.");
+        assertEquals(errorCode, exception.getErrorCode(), "에러 코드가 올바르게 설정되어야 합니다.");
+        assertEquals(message, exception.getMessage(), "메시지가 올바르게 설정되어야 합니다.");
+    }
 }
+
