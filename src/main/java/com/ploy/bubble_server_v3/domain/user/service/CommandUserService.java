7aee59d5d7b45eaa205f34dff411d321c61128f4
한태영 <s46422574@gmail.com>
2025-01-29 20:05:53 +0900

    feat(#15) :: CommandUserService
    updatePassword 메서드와 예외처리를 구현합니다

---
+++ src/main/java/com/ploy/bubble_server_v3/domain/user/service/CommandUserService.java
@@ -0,0 +1,23 @@
+package com.ploy.bubble_server_v3.domain.user.service;
+
+import com.ploy.bubble_server_v3.domain.user.domain.Users;
+import com.ploy.bubble_server_v3.domain.user.presentation.dto.UpdatePasswordRequest;
+import com.ploy.bubble_server_v3.domain.user.service.implementation.UserReader;
+import com.ploy.bubble_server_v3.domain.user.service.implementation.UserUpdater;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CommandUserService {
+
+    private final UserReader userReader;
+    private final UserUpdater userUpdater;
+
+    public void updatePassword(Long id, UpdatePasswordRequest request) {
+        Users user = userReader.findById(id);
+        userUpdater.updatePassword(user, request.password());
+    }
+}
