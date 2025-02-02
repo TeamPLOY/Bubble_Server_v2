b52a5379b064db67be4eeefe98f5574667525728
noahmik <s46422574@gmail.com>
2025-02-03 07:05:37 +0900

    feat(#14) :: createUser
    Users 객체를 생성하는 create 메서드를 구현합니다

---
+++ src/main/java/com/ploy/bubble_server_v3/domain/auth/service/implementation/AuthCreator.java
@@ -0,0 +1,22 @@
+package com.ploy.bubble_server_v3.domain.auth.service.implementation;
+
+import com.ploy.bubble_server_v3.domain.auth.presentation.dto.request.SignUpRequest;
+import com.ploy.bubble_server_v3.domain.user.domain.Users;
+import com.ploy.bubble_server_v3.domain.user.domain.vo.WashingRoom;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthCreator {
+
+    public Users create(SignUpRequest req, WashingRoom washingRoom) {
+        return Users.builder()
+                .email(req.email())
+                .password(new BCryptPasswordEncoder().encode(req.password()))
+                .name(req.name())
+                .stuNum(req.stuNum())
+                .roomNum(req.roomNum())
+                .washingRoom(washingRoom)
+                .build();
+    }
+}
