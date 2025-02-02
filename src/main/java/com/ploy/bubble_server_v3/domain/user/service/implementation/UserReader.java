1bd554238754404bda89e43463eaad2f359b00b5
noahmik <s46422574@gmail.com>
2025-02-03 04:31:47 +0900

    refactor(#17) :: getWashingRoomFromRoomNum 모듈화
    getWashingRoomFromRoomNum를 UserReader에 모듈화 시키는걸로 개선했습니다

--- src/main/java/com/ploy/bubble_server_v3/domain/user/service/implementation/UserReader.java
+++ src/main/java/com/ploy/bubble_server_v3/domain/user/service/implementation/UserReader.java
@@ -2,6 +2,7 @@ package com.ploy.bubble_server_v3.domain.user.service.implementation;
 
 import com.ploy.bubble_server_v3.domain.user.domain.Users;
 import com.ploy.bubble_server_v3.domain.user.domain.repository.UsersRepository;
+import com.ploy.bubble_server_v3.domain.user.domain.vo.WashingRoom;
 import com.ploy.bubble_server_v3.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,11 @@ public class UserReader {
     public Users findById(Long id) {
         return userRepository.findById(id)
                 .orElseThrow(UserNotFoundException::new);
+    }
+
+    public WashingRoom getWashingRoomFromRoomNum(String roomNum) {
+        String prefix = roomNum.substring(0, 1);
+        int roomNumber = Integer.parseInt(roomNum.substring(1));
+        return WashingRoom.findWashingRoom(prefix, roomNumber);
     }
 }
