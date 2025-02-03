2ab52b71b2ff5973e5cbee6584425266814c72bb
noahmik <s46422574@gmail.com>
2025-02-04 04:19:54 +0900

    fix(#14) :: SignUp 메서드
    더티 체킹시 새로운 컬럼 저장 안되는 에러 해결

--- src/main/java/com/ploy/bubble_server_v3/domain/auth/service/implementation/AuthCreator.java
+++ src/main/java/com/ploy/bubble_server_v3/domain/auth/service/implementation/AuthCreator.java
@@ -2,23 +2,30 @@ package com.ploy.bubble_server_v3.domain.auth.service.implementation;
 
 import com.ploy.bubble_server_v3.domain.auth.presentation.dto.request.SignUpRequest;
 import com.ploy.bubble_server_v3.domain.user.domain.Users;
+import com.ploy.bubble_server_v3.domain.user.domain.repository.UsersRepository;
 import com.ploy.bubble_server_v3.domain.user.domain.vo.Role;
 import com.ploy.bubble_server_v3.domain.user.domain.vo.WashingRoom;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthCreator {
+    private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Users create(SignUpRequest req, WashingRoom washingRoom) {
-        return Users.builder()
+        Users user = Users.builder()
                 .email(req.email())
-                .password(new BCryptPasswordEncoder().encode(req.password()))
+                .password(passwordEncoder.encode(req.password()))
                 .name(req.name())
                 .stuNum(req.stuNum())
                 .roomNum(req.roomNum())
                 .washingRoom(washingRoom)
                 .role(Role.USER)
                 .build();
+
+        return usersRepository.save(user);
     }
 }
