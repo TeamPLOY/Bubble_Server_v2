ef012a249f0a22584b6a84d734386a71daf34a89
noahmik <s46422574@gmail.com>
2025-02-07 23:36:32 +0900

    refactor(#22) :: UpdatePasswordRequest
    UpdatePasswordRequest에서 현재 비밀번호 컬럼을 추가합니다

--- src/main/java/com/ploy/bubble_server_v3/domain/user/presentation/dto/UpdatePasswordRequest.java
+++ src/main/java/com/ploy/bubble_server_v3/domain/user/presentation/dto/UpdatePasswordRequest.java
@@ -1,9 +1,12 @@
 package com.ploy.bubble_server_v3.domain.user.presentation.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 public record UpdatePasswordRequest(
+        @Schema(description = "현재 비밀번호")
+        @NotBlank String currentPassword,
         @Schema(description = "변경할 비밀번호")
-        String password
+        @NotBlank String newPassword
 ) {
 }
