4f65825e16c7cbcd1f0365cea3cf464b45813903
Yunchan-Oh <oyunchan9@gmail.com>
2025-04-15 15:23:32 +0900

    feat(#35) :: WashingMachineService 기계 정보를 레디스에 저장 및 조회하는 기능을 구현했습니다.

--- src/main/java/com/ploy/bubble_server_v3/domain/machine/service/WashingMachineService.java
+++ src/main/java/com/ploy/bubble_server_v3/domain/machine/service/WashingMachineService.java
@@ -1,5 +1,8 @@
 package com.ploy.bubble_server_v3.domain.machine.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ploy.bubble_server_v3.domain.machine.domain.WashingMachine;
 import com.ploy.bubble_server_v3.domain.machine.exception.WashingMachineDataFetchException;
 import com.ploy.bubble_server_v3.domain.machine.infra.dto.response.WashingMachineResponse;
@@ -8,12 +11,10 @@ import com.ploy.bubble_server_v3.domain.machine.util.DeviceProcessor;
 import com.ploy.bubble_server_v3.domain.machine.util.HeaderProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -24,6 +25,10 @@ public class WashingMachineService {
     private final WashingMachineFeign washingMachineFeign;
     private final HeaderProvider headerProvider;
     private final DeviceProcessor deviceProcessor;
+    private final ObjectMapper objectMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private static final String REDIS_KEY_PREFIX = "washingMachineStatus:";
 
     private static final List<String> SORT_ORDER = Arrays.asList(
             "세탁기1", "세탁기2", "세탁기3", "세탁기4", "건조기1", "건조기2"
@@ -57,6 +62,35 @@ public class WashingMachineService {
 
         return result;
     }
+
+    public void saveWashingMachines(List<WashingMachine> washingMachineList) {
+        String redisKey = REDIS_KEY_PREFIX + "all";
+        try {
+            String json = objectMapper.writeValueAsString(washingMachineList);
+            redisTemplate.opsForValue().set(redisKey, json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON 직렬화 실패", e);
+        }
+    }
+
+    public List<WashingMachine> getAllWashingMachinesFromRedis() {
+        String redisKey = REDIS_KEY_PREFIX + "all";
+        Object value = redisTemplate.opsForValue().get(redisKey);
+
+        log.info("레디스 조회 성공 : " + value);
+
+        try {
+
+            if (value instanceof String json) {
+                List<WashingMachine> list = objectMapper.readValue(json, new TypeReference<>() {});
+                return list;
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON 역직렬화 실패", e);
+        }
+        return null;
+    }
+
 
     private WashingMachineResponse fetchWashingMachineData() {
         try {
